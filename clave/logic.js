3169b28814da3f8523c7d1273a3e390335e98586
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 <vpadillahoyos@gmail.com>
2024-01-27 11:03:32 -0500

    Actualizacion logic.js

--- clave/logic.js
+++ clave/logic.js
@@ -1,3 +1,5 @@
+let clavecontainer = document.getElementById("clave");
+
 function mezclarArray(Coleccion) {
     for (var i = Coleccion.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -14,4 +16,18 @@ let Coleccion = [1,2,3,4,5,6,7,8,9,0];
  
 let ColeccionMezclada = mezclarArray(Coleccion);
  
-console.log(ColeccionMezclada);
+const mappedNumbers = ColeccionMezclada.map((number, index) => {
+    /* return `<div class="project-card" key=${index}>
+                  <img src=${user.avatar} alt="user profile image" />
+                  <div class="details">
+                   <p>${user.name}</p>
+                   <p>${user.home_address}</p>
+                   <p>${user.office_address}</p>
+                 </div>
+            </div>`; */
+    return `<tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        </tr>`;
+  });
